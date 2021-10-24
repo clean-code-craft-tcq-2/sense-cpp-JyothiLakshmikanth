071f086bcebaba0ac6b70ee1fbc26e14d96e707c
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2021-10-24 17:51:14 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <numeric>
 
+using namespace std;
+
 struct Stats
 {
     float average = NAN;
@@ -11,4 +13,14 @@ struct Stats
 
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& data);
+}
+
+class StatsAlerter
+{
+    private:
+    float thresholdValue = NAN;
+    std::vector<IAlerter*> alerters;
+    public:
+    StatsAlerter (float value, std::vector<IAlerter*> a_v): thresholdValue(value), alerters(a_v){}
+    void checkAndAlert(const std::vector<float>& data);
 }
