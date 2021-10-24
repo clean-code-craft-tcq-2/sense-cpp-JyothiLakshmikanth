d6b96474402c1f8544c0271ab94fa00407fd2f39
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2021-10-24 17:15:54 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,12 +2,12 @@
 
 struct Stats
 {
-    float average = 0.0;
-    float max = 0.0;
-    float min = 0.0;
-}obj1;
+    float average = NAN;
+    float max = NAN;
+    float min = NAN;
+};
 
 namespace Statistics {
     template <typename T>
-    Stats ComputeStatistics(const std::initializer_list<T>& );
+    Stats ComputeStatistics(const std::vector<T>& data);
 }
