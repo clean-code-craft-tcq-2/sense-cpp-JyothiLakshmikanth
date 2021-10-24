0e12fa76f78032d6260a9394a301ded3015e09ea
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2021-10-24 20:57:02 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -14,14 +14,40 @@ struct Stats
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& data);
 }
-
-/*class StatsAlerter
+class IAlerter
 {
-    private:
-    float thresholdValue = NAN;
-    std::vector<IAlerter*> alerters;
+public:
+virtual void setEmailSend(bool value) {};
+virtual void setLEDGlow(bool value) {};
+};
+
+class EmailAlert : public IAlerter
+{
+    public:
+    bool emailsend = false;
+    void setEmailSend(bool value)
+    {
+      emailsend = value;
+    }
+};
+class LEDAlert : public IAlerter
+{
+    public:
+    bool ledglow = false;
+    void setLEDGlow(bool value)
+    {
+       ledglow = value;
+    }
+};
+
+class StatsAlerter
+{
     public:
     StatsAlerter (float value, std::vector<IAlerter*> a_v): thresholdValue(value), alerters(a_v){}
     void checkAndAlert(const std::vector<float>& data);
+    private:
+    float thresholdValue = NAN;
+    std::vector<IAlerter*> alerters;
 };
-*/
+
+
