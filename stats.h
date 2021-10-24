d278b1a099f5dedfa276a28ab8838f448fb0d109
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2021-10-24 17:56:25 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -15,7 +15,7 @@ namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& data);
 }
 
-class StatsAlerter
+/*class StatsAlerter
 {
     private:
     float thresholdValue = NAN;
@@ -23,4 +23,5 @@ class StatsAlerter
     public:
     StatsAlerter (float value, std::vector<IAlerter*> a_v): thresholdValue(value), alerters(a_v){}
     void checkAndAlert(const std::vector<float>& data);
-}
+};
+*/
