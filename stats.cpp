33b9db2c9c01e88f1f1588927cddf6f0e7401aa8
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2021-10-24 18:23:46 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,43 +1,19 @@
 #include "stats.h"
+#include <algorithm>
 
 float findAverage(const std::vector<float>& givenData)
 {
+    ::std::cout<<"findAverage"<<endl;
     float totalSum=0;
-    size_t dataSize = givenData.size();
+    int dataSize = givenData.size();
+    ::std::cout<<"findAverage dataSize - "<<dataSize<<endl;
     ::std::vector<float>::const_iterator givenDataIt = givenData.begin();
     for(;givenDataIt != givenData.end(); ++givenDataIt)
     {
-        totalSum = totalSum + *givenDataIt;
+        ::std::cout<<"findAverage givenDataIt - "<<*givenDataIt<<endl;
+        totalSum += *givenDataIt;
     }
     return static_cast<float>(totalSum/dataSize);
-}
-
-float finMax(const std::vector<float>& givenData)
-{
-    ::std::vector<float>::const_iterator givenDataIt = givenData.begin();
-    float maxValue = *givenDataIt;
-    for(;givenDataIt != givenData.end(); ++givenDataIt)
-    {
-        if(maxValue < *(++givenDataIt))
-        {
-            maxValue = *(++givenDataIt);
-        }
-    }
-   return maxValue;
-}
-
-float finMin(const std::vector<float>& givenData)
-{
-    ::std::vector<float>::const_iterator givenDataIt = givenData.begin();
-    float minValue = *givenDataIt;
-    for(;givenDataIt != givenData.end(); ++givenDataIt)
-    {
-        if(minValue > *(++givenDataIt))
-        {
-            minValue = *(++givenDataIt);
-        }
-    }
-    return minValue;
 }
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& givenData) 
@@ -47,8 +23,8 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& givenData)
     if(!givenData.empty())
     {
         obj1.average = findAverage(givenData);
-        obj1.max = finMax(givenData);
-        obj1.min = finMin(givenData);
+        obj1.max = *max_element(givenData.begin(), givenData.end());
+        obj1.min = *min_element(v.begin(), v.end());
     }
     return obj1;
 }
