b307b25ac38d9f543174e061a9b539b7fef1d489
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2021-10-24 18:25:56 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,17 +3,15 @@
 
 float findAverage(const std::vector<float>& givenData)
 {
-    ::std::cout<<"findAverage"<<endl;
     float totalSum=0;
     int dataSize = givenData.size();
-    ::std::cout<<"findAverage dataSize - "<<dataSize<<endl;
     ::std::vector<float>::const_iterator givenDataIt = givenData.begin();
     for(;givenDataIt != givenData.end(); ++givenDataIt)
     {
         ::std::cout<<"findAverage givenDataIt - "<<*givenDataIt<<endl;
         totalSum += *givenDataIt;
     }
-    return static_cast<float>(totalSum/dataSize);
+    return (totalSum/static_cast<float>(dataSize));
 }
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& givenData) 
@@ -24,7 +22,7 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& givenData)
     {
         obj1.average = findAverage(givenData);
         obj1.max = *max_element(givenData.begin(), givenData.end());
-        obj1.min = *min_element(v.begin(), v.end());
+        obj1.min = *min_element(givenData.begin(), givenData.end());
     }
     return obj1;
 }
